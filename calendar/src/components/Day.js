59765a88f8e2bb084ea48a2d7220f6c7dd859164
Aila Lu <ailalutw@gmail.com>
2023-02-15 10:17:19 -0800

    tailwindcss does not apply

--- calendar/src/components/Day.js
+++ calendar/src/components/Day.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function Day({day}) {
+export default function Day({day, rowIdx}) {
   return (
-    <div>
-      {day.format()}
-    </div>
+    <div className="border border-gray-200 flex flex-col">
+      <header className="flex flex-col items-center">
+        {rowIdx === 0 && ( //only show the first row Mon, Tue so on
+          <p className="text-sm mt-1">
+            {day.format("ddd").toUpperCase()}
+          </p>
+        )}
+        <p
+          className="text-sm p-1 my-1 text-center">{day.format("DD")}
+        </p>
+      </header>
+      </div>
   )
 }
--- calendar/tailwind.config.js
+++ calendar/tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
@@ -9,3 +10,4 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/forms")],
 }
+
