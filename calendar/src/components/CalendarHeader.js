7014c1f44b8c4bcf405323bf18bfd4ac6f1df4e8
Aila Lu <ailalutw@gmail.com>
2023-02-20 13:28:00 -0800

    setting context

--- calendar/src/components/CalendarHeader.js
+++ calendar/src/components/CalendarHeader.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import dayjs from 'dayjs'
 
 export default function CalendarHeader() {
   return (
-    <div>
-      
-    </div>
+    <header className="px-4 py-2 flex items-center">
+      <h1 className="mr-10 text-xl text-gray-500 fond-bold">
+        Retreat
+      </h1>
+      <button
+        // onClick={handleReset}
+        className="border rounded py-2 px-4 mr-5"
+      >
+        Present
+      </button>
+      <button>
+      {/* // onClick={handlePrevMonth} */}
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          {/* chevron_left */}
+        </span>
+      </button>
+      <button>
+      {/* //  onClick={handleNextMonth} */}
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          {/* chevron_right */}
+        </span>
+      </button>
+      {/* <h2 className="ml-4 text-xl text-gray-500 font-bold">
+        {dayjs(new Date(dayjs().year(), monthIndex)).format(
+          "MMMM YYYY"
+        )}
+      </h2> */}
+    </header>
   )
 }
--- calendar/src/components/Day.js
+++ calendar/src/components/Day.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import dayjs from "dayjs";
 
 export default function Day({day, rowIdx}) {
+  function getCurrentDayClass() {
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+      ? "bg-pink-400 text-white rounded-full w-7"
+      : "";
+  }
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
@@ -10,7 +16,9 @@ export default function Day({day, rowIdx}) {
           </p>
         )}
         <p
-          className="text-sm p-1 my-1 text-center">{day.format("DD")}
+          className={`text-sm p-1 my-1 text-center  ${getCurrentDayClass()}`}
+        >
+          {day.format("DD")}
         </p>
       </header>
       </div>
