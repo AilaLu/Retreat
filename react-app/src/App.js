d0e4faf8bcadd0327933577702307bdcbdb3f7af
Aila Lu <ailalutw@gmail.com>
2023-09-06 16:16:11 -0700

    fix file structure

---
+++ react-app/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import LoginFormPage from "./components/LoginFormPage";
+import { authenticate } from "./store/session";
+import Navigation from "./components/Navigation";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Calendar from 'react-calendar'
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(authenticate()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+        <ProtectedRoute path='/tasks'>
+        <Calendar />
+        </ProtectedRoute>
+          <Route path="/login" >
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
+  );
+}
+
+export default App;
