a20f28268f53c6f18eec1a19887423d73f44f275
Aila Lu <ailalutw@gmail.com>
2023-10-08 13:00:02 -0700

    footer css

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,15 +6,21 @@ import flask_icon from "./flask_icon.png";
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="team-section">
+    <footer>
+      <div className="footer-container">
         <div className="creator">
-          <h2>Creator</h2>
+          <h4>Creator</h4>
+          <a
+              href="https://www.linkedin.com/in/ailalutw/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
           <img
             className="member-image"
             src="https://media.licdn.com/dms/image/C4E03AQHw8P2euo3WZQ/profile-displayphoto-shrink_200_200/0/1517638025526?e=1698883200&v=beta&t=lnYjzvYCaP07-P7VWIQKpe-kwv-p3pCzFJhImAhd2fs"
             alt="Team Member 1"
           />
+             </a>
           <div className="member-name">Aila Lu</div>
           <div>
             <a
@@ -38,100 +44,86 @@ function Footer() {
           </div>
         </div>
         <div className="stack">
+          <h4>Stack</h4>
           <div className="links">
-            <i class="fa-brands fa-react fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                React
-              </span>
-            </i>
+            <div className="first-row">
+              <i class="fa-brands fa-react fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  React
+                </span>
+              </i>
+              <i className="hover-text-img">
+                <img
+                  className="fa-brands-redux-icon hover-text"
+                  src={redux_icon}
+                ></img>
+                <span id="topTip-img" className="tooltip-text-img">
+                  Redux
+                </span>
+              </i>
+              <i className="hover-text-img">
+                <img
+                  className="fa-brands-redux-icon hover-text"
+                  src={flask_icon}
+                ></img>
+                <span id="topTip-img" className="tooltip-text-img">
+                  Flask
+                </span>
+              </i>
+              <i class="fa-solid fa-database fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  SQLAlchemy
+                </span>
+              </i>
+              <i class="fa-brands fa-python fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  Python
+                </span>
+              </i>
+            </div>
 
-            <i className="hover-text-img">
-              <img
-                className="fa-brands-redux-icon hover-text"
-                src={redux_icon}
-              ></img>
-              <span id="topTip-img" className="tooltip-text-img">
-                Redux
-              </span>
-            </i>
-
-            <i className="hover-text-img">
-              <img
-                className="fa-brands-redux-icon hover-text"
-                src={flask_icon}
-              ></img>
-              <span id="topTip-img" className="tooltip-text-img">
-                Flask
-              </span>
-            </i>
-
-            <i class="fa-solid fa-database fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                SQLAlchemy
-              </span>
-            </i>
-
-            <i class="fa-brands fa-python fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                Python
-              </span>
-            </i>
-
-            {/* <i class="fa-solid fa-flask fa-xl hover-text">
-                <span id='topTip' className='tooltip-text'>Flask</span>
-                </i> */}
-
-            <i class="fa-brands fa-node-js fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                Node
-              </span>
-            </i>
-
-            <i class="fa-brands fa-square-js fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                Javascript
-              </span>
-            </i>
-
-            <i class="fa-brands fa-html5 fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                HTML5
-              </span>
-            </i>
-
-            <i class="fa-brands fa-css3-alt fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                CSS3
-              </span>
-            </i>
-
-            {/* <i class="fa-brands fa-aws fa-xl hover-text">
-                <span id='topTip' className='tooltip-text'>Amazon</span>
-                </i> */}
-
-            <i class="fa-brands fa-git-alt fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                Git
-              </span>
-            </i>
-
-            <i class="fa-brands fa-square-github fa-xl hover-text">
-              <span id="topTip" className="tooltip-text">
-                GitHub
-              </span>
-            </i>
-
+            <div className="second-row">
+              <i class="fa-brands fa-node-js fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  Node
+                </span>
+              </i>
+              <i class="fa-brands fa-square-js fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  Javascript
+                </span>
+              </i>
+              <i class="fa-brands fa-html5 fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  HTML5
+                </span>
+              </i>
+              <i class="fa-brands fa-css3-alt fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  CSS3
+                </span>
+              </i>
+              <i class="fa-brands fa-git-alt fa-xl hover-text">
+                <span id="topTip" className="tooltip-text">
+                  Git
+                </span>
+              </i>
+            </div>
+          </div>
+        </div>
+        <div className="github">
+          <h4>GitHub Repo</h4>
             <a
               href="https://github.com/AilaLu/Retreat"
               target="_blank"
-              rel="na noreferrer"
+              rel="noopener noreferrer"
             >
-              GitHub Repo
+            <img className="retreat-github-link" width="96" height="96" src="https://img.icons8.com/color/96/github-2.png" alt="github-2"/>
             </a>
           </div>
-        </div>
-        <div className="stack"> © 2023 Retreat - inspired by DailyBean</div>
       </div>
+
+      <div className="retreat-inspired-by"> © 2023 Retreat - inspired by DailyBean</div>
     </footer>
   );
 }
